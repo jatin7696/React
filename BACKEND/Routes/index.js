443a71edc415f738a16f873ff23e6a3ff273f414
jatin <jatin@tru.agency>
2022-07-26 15:47:05 +0530

    done

--- BACKEND/Routes/index.js
+++ BACKEND/Routes/index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 require("../db/config");
-
-const Razorpay = require("razorpay");
-
-const Order = require("../db/order");
 const log = require("../controller/login");
 const signup = require("../controller/signup");
 const addToCart = require("../controller/addToCart");
@@ -25,57 +21,7 @@ router.post("/create-order", checkout.createOrder);
 router.get("/get-razorpay-key", checkout.getrazorpaykey);
 router.post("/pay-order", checkout.payOrder);
 
-router.post("/create-order", async (req, res) => {
-  console.log("this is requestfororder  ", req.body);
-  try {
-    var instance = new Razorpay({
-      key_id: process.env.Razorpay_Key,
-      key_secret: process.env.Razorpay_secret,
-    });
-
-    let options = {
-      amount: req.body.amount, // amount in the smallest currency unit
-      currency: "INR",
-    };
-    const order = await instance.orders.create(options, function (err, order) {
-      console.log("orders === ", order);
-      if (order == null) {
-        console.log("underidconditoion", order);
-        return res.status(500).send("some error occured");
-      }
-      res.send(order);
-    });
-  } catch (error) {
-    console.log(error, "error aa gaya");
-  }
-});
-
-router.get("/get-razorpay-key", (req, res) => {
-  res.send({ key: "rzp_test_AHarLPM8Zl6Anf" });
-});
-
-router.post("/pay-order", async (req, res) => {
-  try {
-    const { amount, razorpayPaymentId, razorpayOrderId, razorpaySignature } =
-      req.body;
-    const newOrder = Order({
-      isPaid: true,
-      amount: amount,
-      razorpay: {
-        orderId: razorpayOrderId,
-        paymentId: razorpayPaymentId,
-        signature: razorpaySignature,
-      },
-    });
-    await newOrder.save();
-    res.send({
-      msg: "Payment was successfull",
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-});
+module.exports = router;
 
 // router.get("/search/:key", async (req, resp) => {
 //   // console.log("thisissearchhhhhhhhhhhhhhhhhh", req);
@@ -183,5 +129,3 @@ router.post("/pay-order", async (req, res) => {
 //   //cartProduct = req.body._id;
 //   //console.log("cartProduct === >  ", cartProduct);
 // });
-
-module.exports = router;
--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const bcrypt = require("bcrypt");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 const PORT = 8080;
 
 app.use("/api", require("./Routes/index"));
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
